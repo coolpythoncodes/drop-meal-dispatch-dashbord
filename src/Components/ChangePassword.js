0dbfc3d2422a76b707fa2da503b0891f8f7dc868
coolpythoncodes <coolpythoncodes@gmail.com>
2021-02-28 06:57:10 +0100

    change password  popup component

---
+++ src/Components/ChangePassword.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import '../Sass/ChangePassword.scss';
+import HighlightOffIcon from '@material-ui/icons/HighlightOff';
+import Button from './Button';
+
+const ChangePassword = ({close, handleChangePassword, showChangePasswordPopup }) => {
+    return (
+        <div className='change__password' style={showChangePasswordPopup ? {display:'block'} : {display:'none'}}>
+            <div className="container">
+                <div className="top">
+                    <h1>Change Password</h1>
+                    <HighlightOffIcon
+                        style={{ color: '#F0660C', fontSize: 40 }}
+                        onClick={handleChangePassword} 
+                    />
+                    {/* <img src={CloseButton} alt=""/> */}
+                </div>
+                <div className="form">
+                    <form action="">
+                        <div className="name">
+                            <label>New password</label>
+                            <input className='change__passwordInput' type="password" name="" id="name"/>
+                        </div>
+                        <div className="email">
+                            <label>Confirm password</label>
+                            <input className='change__passwordInput' type="password" name="" id="email"/>
+                        </div>
+                        <Button 
+                            title='CHANGE PASSWORD'
+                            color='#FBFBFB'
+                            background='#0079EC'
+                            height='32px' 
+                            style={{
+                                marginTop:'25px',
+                                marginBottom:'92px',
+                            }}                         
+                        />
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default ChangePassword;
